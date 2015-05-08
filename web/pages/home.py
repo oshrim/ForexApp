c48dd7c6bf8ac032cce5e771eb975a015c5e3daf
yaakov300 <yaacov300@gmail.com>
2015-05-08 20:00:21 +0300

    create home.html home.py and css files

---
+++ web/pages/home.py
@@ -0,0 +1,15 @@
+__author__ = 'Oshri&Yaacov'
+
+from google.appengine.ext.webapp import template
+
+import webapp2
+
+class IndexHandler(webapp2.RequestHandler):
+    def get(self):
+        template_params = {}
+        html = template.render("web/templates/home.html", template_params)
+        self.response.write(html)
+
+app = webapp2.WSGIApplication([
+    ('/',IndexHandler)
+], debug=True) 
